d544f9d88e01094cfd90c9e55becb428beac6db4
anuj paul <paulanuj27@gmail.com>
2025-11-15 04:49:54 +0600

    Ading CSS using by object property

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,16 @@ function App() {
 // Function rule is start to the function name is capital letter
 function Student ()
 {
+  const obj = {
+      backgroundColor: 'rgb(84, 84, 230)',
+      margin: '10px',
+      padding: '10px',
+      borderRadius: '10px',
+  }
   return <div>
     <h2>Student Details</h2>
     <p className='btn'>Name: Shyam</p>
-    <p>Roll: 4512141</p>
+    <p style={obj}>Roll: 4512141</p>
   </div>
 }
 
