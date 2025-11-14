9906abc4986b802624ea5fd77e210895f3e1e5d6
anuj paul <paulanuj27@gmail.com>
2025-11-15 04:44:51 +0600

    First Function create and render in react

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,19 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
-      
+      <Student></Student>
     </>
   )
+}
+
+// Function rule is start to the function name is capital letter
+function Student ()
+{
+  return <div>
+    <h2>Student Details</h2>
+    <p>Name: Shyam</p>
+    <p>Roll: 4512141</p>
+  </div>
 }
 
 
