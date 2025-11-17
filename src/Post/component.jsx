897e27918555c3286b8be3f42aa2aa2328b2a703
anuj paul <paulanuj27@gmail.com>
2025-11-18 05:10:13 +0600

    Adding Event Lisnear from another component

--- src/Post/component.jsx
+++ src/Post/component.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import './style.css'
 
 
-function ShowPost ({post})
+function ShowPost ({post,handleMarkVisited})
 {
     const {title} = post;  
     
@@ -16,26 +16,38 @@ function ShowPost ({post})
     return <div className={`btn ${visited? 'back':''}`}>
             <p>{title}</p>
             <button onClick={handleVisited}>{visited? 'Visited':'Not Visit'}</button>
+            <br />
+            <button onClick={()=>handleMarkVisited(post)}>Mark Visited</button>
 
         </div>
 }
 
 const Component = () => {
 
-    const [posts, setPost] = useState([])
+    const [posts, setPost] = useState([]);
+    const [markVisited, setMarkVisited] = useState([]);  
 
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/photos')
         .then(res=> res.json())
         .then(data=> setPost(data))
-    },[])
+    },[]);
+
+    const handleMarkVisited = title =>
+    {
+        setMarkVisited([...markVisited,title])
+    }
 
     return (
         <div > 
             <h2>Hello render system in React</h2>
+            <h4>Visited Country: {markVisited.length}</h4>
+            {
+                markVisited.map(mark=> <li>{mark.id}</li> )
+            }
             <div className="container">
                 {
-                    posts.map(post=> <ShowPost post ={post} key={post.id}></ShowPost> )
+                    posts.map(post=> <ShowPost handleMarkVisited={handleMarkVisited} post ={post} key={post.id}></ShowPost> )
                 }
             </div>
         </div>
