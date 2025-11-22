5f2353a3cc7d2ea3f37f85203535b898c1d6a325
anuj paul <paulanuj27@gmail.com>
2025-11-23 05:42:59 +0600

    Add to CoinClaim

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
-    <Header></Header>
     <Main></Main>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,33 @@
-import logo from '../assets/logo1.png'
+import { useState } from 'react';
+import logo from '../assets/logo.png'
+import Header from './Header';
+import DollarIcon from '../assets/dollar.png'
 const Navbar = () => {
+    const [coin,setCoin] = useState(0)
+
+    function handleCoin ()
+    {
+        setCoin(coin+100)
+    }
+
     return (
-        <div className="w-10/12 flex justify-between my-10 mx-auto">
+        <section>
+            <div className="w-10/12 flex justify-between my-10 mx-auto">
             <img className='w-15' src={logo} alt="" />         
             <div className="flex gap-10 items-center">
                 <p>Home</p>
                 <p>Photos</p>
                 <p>Teams</p>
                 <p>Schedules</p>
-                <button className='btn btn-secondary'> Coin</button>
+                <button className='btn btn-secondary'>
+                    <img className='w-5' src={DollarIcon} alt="" />
+                      {coin} Cr</button>
             </div>
         </div>
+        <Header handleCoin={handleCoin}></Header>
+
+        </section>
+        
     );
 };
 
