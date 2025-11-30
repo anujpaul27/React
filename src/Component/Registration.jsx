5a6c26a6387656a09130ca75a086390ecc21d0b9
anuj paul <paulanuj27@gmail.com>
2025-11-30 20:05:24 +0600

    Password Validation

--- src/Component/Registration.jsx
+++ src/Component/Registration.jsx
@@ -1,8 +1,10 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import React from "react";
+import React, { useState } from "react";
 import auth from "../FireBase/firebase_init";
 
 const Registration = () => {
+    const [error, setError] = useState('')
+    const [success, setSuccessMessage] = useState('')
 
     function handleSubmit (e)
     {
@@ -11,12 +13,46 @@ const Registration = () => {
         const password = e.target.password.value;
         // console.log(name, password);
 
+        // Reset status
+        setError('')
+        setSuccessMessage ('')
+
+        // Password Validation 
+        if (password.length < 6)
+        {
+            setError('Password give at last six charecter ')
+            return;
+        }
+        if (!/[a-z]/.test(password))
+        {
+          setError('At least one lower case letter.')
+          return
+        }
+        if (!/[A-Z]/.test(password))
+        {
+          setError('At least one Upper case letter.')
+          return
+        }
+        if (!/[!@#$%^&*]/.test(password))
+        {
+          setError('At least One Special Character')
+          return;
+        }
+        if (!/\d/.test(password))
+        {
+          setError('At last one number')
+          return;
+        }
+
+
         createUserWithEmailAndPassword(auth,email,password)
         .then(result=> {
-            console.log('Registration successful');
+            setSuccessMessage('Registration successful');
         })
         .catch(error=>{
             console.log(error);
+            setError(error.message)
+            setSuccessMessage('')
         })
     }
 
@@ -27,7 +63,7 @@ const Registration = () => {
         <form onSubmit={handleSubmit}>
           <fieldset className="fieldset">
             {/* <label className="label">Email</label> */}
-            <input type="email" className="input" placeholder="Email" name='email' />
+            <input type="email" className="input" placeholder="Email" name='email' required />
             {/* <label className="label">Password</label> */}
             <input type="password" className="input" placeholder="Password" name='password'/>
             <div>
@@ -36,6 +72,12 @@ const Registration = () => {
             <button className="btn btn-accent w-1/3">Login</button>
           </fieldset>
         </form>
+        {
+            error && <p className="text-sm text-red-400 mt-5">{error}</p>
+        }
+        {
+            success && <p className="text-sm text-green-400 mx-auto mt-5">{success}</p>
+        }
       </div>
     </section>
   );
